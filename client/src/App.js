6eba4d39b8d2b629af956e715b7c9970fc941759
Rohan <reo127.0.0.1@gmail.com>
2022-10-28 21:39:20 +0530

    Add react router dom and created login and register components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import './index.css'
-import TodoLocalhost from './TodoLocalhost';
+import TodoLocalhost from './Container/TodoLocalhost';
+import Navber from './Container/Navber';
+import { Routes, Route } from "react-router-dom";
+import Login from './Container/Login';
+import Register from './Container/Register';
 
-function App(){
-    return(
-       <>
-            <TodoLocalhost />
-       </>
+function App() {
+    return (
+        <>
+           <Navber/>
+        <Routes>
+           <Route path='/' element={<TodoLocalhost/>} />
+           <Route path='/login' element={<Login/>} />
+           <Route path='/register' element={<Register/>} />
+        </Routes>
+        </>
     )
 }
 
---
+++ client/src/Container/Register.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Register = () => {
+    return (
+        <section className="text-gray-400 bg-gray-900 body-font h-[90vh]">
+            <div className="container px-5 py-24 mx-auto flex flex-wrap items-center">
+                <div className="  md:w-1/2 bg-gray-800 bg-opacity-50 rounded-lg p-8 flex flex-col mx-auto w-full mt-10 md:mt-0">
+                    <h2 className="text-white font-medium title-font mb-5 text-3xl text-center">Register</h2>
+                    <div class="relative mb-4">
+                        <label for="full-name" class="leading-7 text-sm text-gray-400">First Name</label>
+                        <input type="text" id="full-name" name="full-name" class="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                    </div>
+                    <div class="relative mb-4">
+                        <label for="full-name" class="leading-7 text-sm text-gray-400">Last Name</label>
+                        <input type="text" id="full-name" name="full-name" class="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                    </div>
+                    <div className="relative mb-4">
+                        <label htmlFor="email" className="leading-7 text-sm text-gray-400">Email</label>
+                        <input type="email" id="email" name="email" className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                    </div>
+                    <div className="relative mb-4">
+                        <label htmlFor="full-name" className="leading-7 text-sm text-gray-400">Password</label>
+                        <input type="password" id="full-name" name="full-name" className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                    </div>
+                    <div className="relative mb-4">
+                        <label htmlFor="full-name" className="leading-7 text-sm text-gray-400">Conform Password</label>
+                        <input type="password" id="full-name" name="full-name" className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                    </div>
+                    <div className='w-full flex justify-center my-3'> <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button></div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Register
